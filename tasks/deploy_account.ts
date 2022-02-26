7f46223eb9fd676b657a3248cb8167aa9e5bba5e
joneslee0918 <joneslee0918@gmail.com>
2022-02-26 16:52:30 +0000

    chore(desiege): module 1 deployment result

---
+++ tasks/deploy_account.ts
@@ -0,0 +1,50 @@
+import { Provider, ec, encode, number } from "starknet";
+import fs from "fs";
+import { getPathBase } from "./helpers";
+
+const DEPLOYMENT_PATH_BASE = getPathBase();
+
+export default async function deployAccount() {
+  if (!fs.existsSync(DEPLOYMENT_PATH_BASE)) {
+    await fs.promises.mkdir(DEPLOYMENT_PATH_BASE, { recursive: true })
+  }
+
+  if (fs.existsSync(`${DEPLOYMENT_PATH_BASE}/OwnerAccount.json`)) {
+    console.log("Deployment already exists")
+    return
+  }
+
+  const keyPair = ec.genKeyPair()
+  const starkKey = ec.getStarkKey(keyPair)
+  const starkKeyInt = number.toBN(encode.removeHexPrefix(starkKey), 16)
+
+  const network = process.env.NETWORK || "georli-alpha"
+  
+  const provider = new Provider({ network: network as any })
+
+  const contract = (await fs.promises.readFile("./artifacts/Account.json")).toString()
+
+  const result = await provider.deployContract(contract, [starkKeyInt.toString()]) // no 0x0 here, just int form
+
+  fs.writeFileSync(`${DEPLOYMENT_PATH_BASE}/OwnerAccount.json`, JSON.stringify({
+    ...result,
+    stark_key_hex: starkKeyInt,
+    private_key: keyPair.getPrivate("hex"),
+  }))
+
+  console.log(`Deployed at ${result.address}`)
+  console.log(`TX: ${result.transaction_hash}`)
+  console.log(`Stark Key ${starkKeyInt}`)
+  console.log(`Private Key ${keyPair.getPrivate("hex")}`)
+  console.log("waiting for transaaction...")
+  try {
+    await provider.waitForTx(result.transaction_hash)
+    const res = await provider.getTransactionStatus(result.transaction_hash)
+    console.log(res);
+  } catch(e){
+    console.error("Error deploying account: ", e )
+  }
+
+}
+
+deployAccount()
---
+++ tasks/helpers/index.ts
@@ -0,0 +1,146 @@
+import { Provider, ec, Signer } from 'starknet'
+import fs from 'fs'
+import { toBN } from 'starknet/dist/utils/number'
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+
+dotenvConfig({ path: resolve(__dirname, "../../.env") });
+
+export const DEPLOYMENT_PATH_BASE = "./minigame-deployments/starknet";
+
+export function getPathBase() {
+  if (process.env.NETWORK && process.env.NETWORK !== "goerli") {
+    return `${DEPLOYMENT_PATH_BASE}-${process.env.NETWORK}`;
+  }
+
+  return `${DEPLOYMENT_PATH_BASE}-goerli`;
+}
+
+export function setupDeploymentDir() {
+  const path_base = getPathBase()
+
+  if (!fs.existsSync(path_base)) {
+    fs.mkdirSync(path_base, { recursive: true })
+  }
+}
+
+export function checkDeployment(contractName: string) {
+  const path_base = getPathBase()
+
+  if (fs.existsSync(`${path_base}/${contractName}.json`)) {
+    throw new Error("Deployment already exists")
+  }
+}
+
+export function getOwnerAccountInt(): string {
+  const path_base = getPathBase()
+
+  if (process.env.OWNER_ACCOUNT) {
+    return BigInt(process.env.OWNER_ACCOUNT).toString()
+  }
+
+  try {  
+    const file = fs.readFileSync(`${path_base}/OwnerAccount.json`)
+
+    const parsed = JSON.parse(file.toString())
+
+    return BigInt(parsed.address).toString()
+  } catch (error) {
+    console.log(`No OWNER_ACCOUNT env variable nor "${path_base}/OwnerAccount.json" provided.`)
+    throw error
+  }
+}
+
+export function writeDeployment(contractAlias: string, result: any) {
+  const path_base = getPathBase()
+
+  fs.writeFileSync(`${path_base}/${contractAlias}.json`, JSON.stringify({
+    ...result,
+  }, null, 4))
+}
+
+export function writeNileDeploymentFile(contractName: string, contractAlias: string, result: any) {
+
+  const deploymentFilename = "./minigame-deployments/goerli.deployments.txt";
+
+  if (fs.existsSync(deploymentFilename)) {
+    fs.appendFileSync(deploymentFilename, `\n${result.address}:artifacts/abis/${contractName}.json:${contractAlias}`)
+  } else {
+    fs.writeFileSync(deploymentFilename, `${result.address}:artifacts/abis/${contractName}.json:${contractAlias}`)
+  }
+}
+
+export function logDeployment(result: any) {
+  console.log(`Deployed at ${result.address}`)
+  console.log(`TX: ${result.transaction_hash}`)
+}
+
+export function getDeployedAddressInt(contractName: string) {
+  const path_base = getPathBase()
+
+  try {
+    const file = fs.readFileSync(`${path_base}/${contractName}.json`)
+
+    const parsed = JSON.parse(file.toString())
+
+    return BigInt(parsed.address).toString()
+  } catch (error) {
+    console.log(`Deployment ${contractName} doesn't exist`)
+  }
+}
+
+export async function deployContract(contractName: string, contractAlias: string, args: string[]) {
+  const network: any = process.env.NETWORK || "georli-alpha"
+  const provider = new Provider({ network })
+
+  console.log("Deploying..." + contractName)
+
+  const contract = (await fs.promises.readFile(`./artifacts/${contractName}.json`)).toString()
+
+  const result = await provider.deployContract(contract, args) // no 0x0 here, just int form
+
+  console.log("Waiting for transaction...")
+  logDeployment(result)
+
+  try {
+    await provider.waitForTx(result.transaction_hash)
+    const res = await provider.getTransactionStatus(result.transaction_hash)
+    writeDeployment(contractAlias, result)
+    writeNileDeploymentFile(contractName, contractAlias, result)
+    console.log(res);
+  } catch(e){
+    console.error("Error Deploying Contract: ", e )
+  }
+
+  return result
+}
+
+export function getSigner() {
+  const path_base = getPathBase()
+  try {  
+    const file = fs.readFileSync(`${path_base}/OwnerAccount.json`)
+
+    const parsed = JSON.parse(file.toString())
+
+    const privKey = process.env.MINIGAME_ARBITER_PRIVATE_KEY;
+
+    if(privKey == undefined){
+      throw new Error("Attempted to call getSigner() with MINIGAME_ARBITER_PRIVATE_KEY being undefined. Set env value in .env or execution environment.")
+    }
+
+    const kp = ec.getKeyPair(toBN(privKey, 16))
+    const p = new Provider({
+      network: "georli-alpha"
+    })
+    const s = new Signer(p, parsed.address, kp )
+    return s;
+
+  } catch( e ) {
+    console.error("Signing error: ", e)
+  }
+}
+
+export function getVersionSuffix() {
+  const now = new Date()
+  return `${now.getFullYear()}${now.getMonth() + 1}${now.getDate()}`
+}
